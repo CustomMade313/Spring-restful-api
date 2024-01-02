a72920c8ba789ee09f225f32604e20db56fa63a9
CustomMade313 <31367091+CustomMade313@users.noreply.github.com>
2024-01-02 16:35:42 +0200

    Add files via upload

---
+++ springboot_tutorial_data/src/main/java/com/custommade/springboot_tutorial_data/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.custommade.springboot_tutorial_data.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+
+}
