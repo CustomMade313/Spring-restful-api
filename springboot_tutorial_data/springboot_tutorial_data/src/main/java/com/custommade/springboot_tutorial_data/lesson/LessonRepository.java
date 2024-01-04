7c53023822a22f9d0b112e49bf829d6fa7eca35b
CustomMade313 <31367091+CustomMade313@users.noreply.github.com>
2024-01-04 11:52:17 +0200

    Delete springboot_tutorial_data/springboot_tutorial_data directory

--- springboot_tutorial_data/springboot_tutorial_data/src/main/java/com/custommade/springboot_tutorial_data/lesson/LessonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.custommade.springboot_tutorial_data.lesson;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface LessonRepository extends CrudRepository<Lesson, String>{
-    public List<Lesson> findByCourseId(String courseId);
-}
--- springboot_tutorial_data/springboot_tutorial_data/src/main/java/com/custommade/springboot_tutorial_data/topic/TopicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.custommade.springboot_tutorial_data.topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String>{
-
-}
